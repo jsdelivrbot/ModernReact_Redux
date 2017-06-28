e0a7256ea9dedf8347894e08a68de345b380b0dc
GaryYongYao <garyyongyao@gmail.com>
2017-06-28 17:10:33 +0800

    28JunCommit
    Until lecture 133

---
+++ Lesson4-React Redux Form/src/components/posts_new.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class PostsNew extends Component {
+    renderTitleField(field) {
+        return (
+            <div className="form-group">
+                <label>Title</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    onChange={field.input.onChange}
+                />
+            </div>
+        );
+    }
+
+    render() {
+        return (
+            <form>
+                <Field
+                    name="title"
+                    component={this.renderTitleField}
+                />
+            </form>
+        );
+    }
+}
+
+export default reduxForm({
+    form: 'PostsNewForm'
+})(PostsNew);
